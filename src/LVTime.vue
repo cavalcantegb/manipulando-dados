8b5ae5ae08aca42f3dacf710971cf0c9f2cb93eb
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-06-07 11:11:22 -0300

    Cap 6 - Primeiras partes ate pag 72

---
+++ src/LVTime.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h3>Buscando rivais</h3>
+        <label>Time:</label>
+        <input type="text" v-model="busca">
+        <p>Rival: {{rival(busca)}}</p>
+
+        <h3>Perfil</h3>
+        <p>Time: {{nome}}</p>
+        <p>Estadio: {{estadio}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LVTime',
+    data() {
+        return {
+            nome: 'Cruzeiro',
+            estadio: 'Mineirao',
+            busca: '',
+        }
+    },
+    methods: {
+        rival(time) {
+            switch( time.toLowerCase() ){
+                case 'corithians':
+                    return 'palmeiras'
+                case 'santos':
+                    return 'sao paulo'
+                case 'flamengo':
+                    return 'vasco'
+                case 'cruzeiro':
+                    return 'em minas eh marmelada'
+                default: 
+                    return 'nao encontrado' 
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
